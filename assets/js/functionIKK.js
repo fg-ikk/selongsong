3a2ceecd93c86dccade18b00effd64bc897521b5
Rizal A <rizal.fg.ikk@gmail.com>
2025-10-21 05:35:31 +0700

    Add Login page

--- assets/js/functionIKK.js
+++ assets/js/functionIKK.js
@@ -7,3 +7,58 @@ const getUserObject = () => (!isEmptyObject(JSON.parse(getUser()))) ? JSON.parse
 function isEmptyObject(obj) {
   return Object.keys(obj).length === 0;
 }
+
+const fetchPlus = (url, options = {}, retries) =>
+  fetch(url, 
+        Object.assign(options,
+                      {retryOptions:  {
+                  maxAttempts: retries,        // Max 3 retries (4 total attempts)
+                  initialDelay: 500,    // Start with 500ms delay
+                  backoffFactor: 2.0, // Double delay each time (500ms, 1s, 2s)
+                  maxAge: 60000,        // Give up after 60 seconds total retry time
+                  retryAfterUnload: true  // Allow retries to continue even if page closes
+                  }
+              })
+            )
+              
+    //fetch(url, options)
+    .then((res) => {
+      if (res.status === 401) {
+          funcLogout();
+      }
+      if (res.ok) {
+        return res.json()
+      }
+      if (retries > 0) {
+        return fetchPlus(url, options, retries - 1)
+      }
+      throw new Error(res.status)
+    })
+    .catch((error) => console.error(error.message));
+
+function indoDate(datetime){
+    return new Date(datetime).toLocaleString('id-ID', {
+                              day: 'numeric',
+                              month: 'short',
+                              year: 'numeric'
+                           })
+}
+
+const addOneDay = (dateString) => {
+  let [day, month, year] = dateString.split('-')
+  const currentDate = new Date(+year, +month - 1, +day)
+  currentDate.setDate(currentDate.getDate()+1);
+  return padZero(currentDate.getDate()) + '-' + padZero(currentDate.getMonth() + 1) + '-' + currentDate.getUTCFullYear();
+}
+
+const currentDate = () => {
+  let currentDate = new Date();
+  return padZero(currentDate.getDate()) + '-' + padZero(currentDate.getMonth() + 1) + '-' + currentDate.getUTCFullYear();
+}
+
+const convertDate = (dateString) => {
+  let currentDate = new Date(dateString);
+  return padZero(currentDate.getDate()) + '-' + padZero(currentDate.getMonth() + 1) + '-' + currentDate.getUTCFullYear();
+}
+
+const padZero = (num) => num < 10 ? '0' + num : num;
